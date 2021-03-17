65f201bb0a4d8505f79e2d7fbd5171c10dc53843
Kavya-uday123 <mca522@rajagiri.edu>
2021-03-17 14:54:08 +0530

    Employee

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -26,6 +26,10 @@
 <br>
 <h5><center>ADD EMPLOYEE</center></h5>
 <br>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
     <table class="table">
     <tr>
         <td>Employee Name</td>
@@ -64,5 +68,8 @@
         <td><button class="btn btn-success">ADD</button></td>
     </tr>
     </table>
+    </div>
+    <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+    </div></div>
 </body>
 </html>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -26,6 +26,10 @@
 <br>
 <h5><center>SEARCH EMPLOYEE</center></h5>
 <br>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
     <table class="table">
         <td>Employee Code</td>
         <td><input type="number" class="form-control"></td>
@@ -35,5 +39,8 @@
         <td><button class="btn btn-success">SEARCH</button></td>
     </tr>
     </table>
+    </div>
+    <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+    </div></div>
 </body>
 </html>
